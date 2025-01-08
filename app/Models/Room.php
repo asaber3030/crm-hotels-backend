c8724eb3a0c40cbed4344034f7703b2061329610
asaber3030 <abdulrahmansaber70@gmail.com>
2025-01-08 21:48:51 +0200

    add HasFactory trait to all models

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Room extends Model
 {
 
-	use SoftDeletes;
+	use SoftDeletes, HasFactory;
 
 	protected $table = 'rooms';
 	protected $fillable = ['hotel_id', 'room_type'];
